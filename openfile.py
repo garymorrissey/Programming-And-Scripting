c7109d081de67a71e9dfeaa76cee32223a823bb9
garymorrissey <36278683+garymorrissey@users.noreply.github.com>
2018-03-28 10:17:05 +0100

    Update openfile.py

--- openfile.py
+++ openfile.py
@@ -1,8 +1,9 @@
-# Gary Morrissey
+# Gary Morrissey, 
 # Week 5 - Formatting Exercise
+# Adapted from http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data
 
-print("Petal Length", "Petal Width", "Sepal Length", "Sepal Width")  
+print("Petal Length", "Petal Width", "Sepal Length", "Sepal Width")  # Column headings
 
-with open ("Data/iris.csv") as f:
+with open ("Data/iris.csv") as f: # Link the csv file
     for line in f:
         print('{:^12} {:^12} {:^12} {:^12}'.format(line.split(',')[0], line.split(',')[1], line.split(',')[2], line.split(',')[3]))
