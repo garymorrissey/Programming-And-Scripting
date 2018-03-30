eeea1e72f4c5eb83423b0e9c7e73d19aafb4071f
garymorrissey <36278683+garymorrissey@users.noreply.github.com>
2018-03-30 13:30:29 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -2,11 +2,11 @@
 # Topic 6 - Factorial of an integer value
 # Adapted from https://www.programiz.com/python-programming/examples/factorial
 
-x = int(input("Please enter an integer:"))
+x = int(input("Please enter a number:"))
 
 factorial = 1
 
-if x < 0: # check if the number is negative, positive
+if x < 0: # check if the number is negative or positive
    print("Sorry, factorial does not exist for negative numbers")
 elif x == 0: # check if the number is zero
    print("The factorial of 0 is 1")
